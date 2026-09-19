e58a9082be3fb21e76e72be16df0a3a3fb1a2ce8
Md Imon Hossain <imon.psy.dc@gmail.com>
2026-09-19 07:34:51 +0530

    Render deployment setup ready

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn neurohealth_project.wsgi
--- neurohealth_project/settings.py
+++ neurohealth_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ft)^fi3dnxu4%mkiqx#29ypv$v_8rl1fm_22kuxe(zoyv$j0)*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,3 +127,7 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
